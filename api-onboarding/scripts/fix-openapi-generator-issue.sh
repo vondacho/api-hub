6a63757635159aa256f5c73d720f058fe338b4c5
Olivier von Dach <olivier.vondach@obya.ch>
2026-07-28 20:05:14 +0200

    fix(openapi): work around for map-typed query params in generated interface api classes

---
+++ api-onboarding/scripts/fix-openapi-generator-issue.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+GENERATED_API_DIR="${1:?usage: script.sh <generated_api_dir>}"
+
+find "$GENERATED_API_DIR" -name "*.java" -exec \
+  sed -i.bak -E 's/@RequestParam\(value = "", required = ([a-z]+)\) @Nullable Map<String, Object> filters/@RequestParam(value = "filters", required = \1) @Nullable Map<String, Object> filters/g' {} \;
+find "$GENERATED_API_DIR" -name "*.bak" -delete
